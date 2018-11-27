5d99ea8aa68428efd9107614a93dc79120aa7bfc
linyang <linyang30>
2018-11-27 15:36:50 +0800

    framework complete

--- test_run/run.py
+++ test_run/run.py
@@ -1,3 +1,18 @@
 import unittest
+from common.common_func import get_time
+from BSTestRunner import BSTestRunner
+import logging
 
-discover = unittest.defaultTestLoader.discover('../test_case', pattern='combination_1.py')
+test_dir='../test_case'
+report_dir='../test_reports'
+
+discover=unittest.defaultTestLoader.discover(test_dir, pattern='combination_1.py')
+
+report_name=report_dir + '/' + get_time() + ' test_report.html'
+
+
+
+with open(report_name,'wb') as f:
+    runner=BSTestRunner(stream=f,title='sdk2.0 Test Report',description='sdk2.0 automatic test report')
+    logging.info('start run test case...')
+    runner.run(discover)
